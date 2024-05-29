13e3f26f5d8586ee6dc8cdce1ae42754eb200db5
Riya Tripathi <riyatripathi1511@gmail.com>
2024-05-29 23:02:09 +0530

    arrow function

---
+++ JAVASCRIPT/ES6/Arrowfunc.js
@@ -0,0 +1,34 @@
+//ARROW-FUNCTION
+// Syntax :
+// const functionName = (parameters) => {
+//     //function body
+// }
+// const greet = () => clg("hello word");
+
+// const sqare = x => x*x;
+
+// function add(a, b) {
+//     // console.log(a + b);
+//     return a + b;
+// }
+// console.log(add(10, 20));
+
+const addA = (a, b) => {
+  return a + b;
+}
+console.log(addA(20, 30));
+
+const sumArr = (a, b) => a + b;
+console.log(sumArr(10, 20));
+
+function double(a) {
+  return a * 3
+}
+
+const doubleA = a => a * 2;
+console.log(doubleA(10));
+
+document.addEventListener("click", () => console.log("button was clicked.."));
+
+
+
